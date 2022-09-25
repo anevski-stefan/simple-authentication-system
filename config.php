d6b83b5eba832ced5c66862287f7b2133bc146a3
anevski-stefan <anevskistefan84@gmail.com>
2022-09-25 04:04:24 +0200

    Changed config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-$pass = "";
+$server = "us-cdbr-east-06.cleardb.net";
+$user = "b1cd55ef9b037b";
+$pass = "40851f57";
 $database = "register_login_db";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
